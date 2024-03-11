5aa9728a064382e774f015f20fd47de1f75614c6
roads <roads@roads.pw>
2024-03-10 22:49:13 -0700

    add imu

--- js/script.js
+++ js/script.js
@@ -427,7 +427,7 @@ scene.background = new THREE.Color('black');
 
 {
   const objLoader = new OBJLoader();
-  objLoader.load('assets/bunny.obj', (root) => {
+  objLoader.load('assets/IMU.obj', (root) => {
     bunny = root;
     scene.add(root);
   });
